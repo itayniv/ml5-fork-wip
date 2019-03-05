c0ebce88dd5a97d57c23df10a136be17ed506713
Itay Niv <itayniv@gmail.com>
2019-03-05 17:02:08 -0500

    minor changes

--- src/Sentiment/index.js
+++ src/Sentiment/index.js
@@ -10,6 +10,10 @@ Sentiment
 
 import * as tf from '@tensorflow/tfjs';
 
+// load loader.js from local directory
+import * as loader from './loader';
+
+
 
 //load model and metadata from tensorflow (or locally?)
 
@@ -24,3 +28,34 @@ const HOSTED_URLS = {
 class PredictSentiment{
 
 }
+
+
+
+async function setupSentiment() {
+  if (await loader.urlExists(HOSTED_URLS.model)) {
+    ui.status('Model available: ' + HOSTED_URLS.model);
+    const button = document.getElementById('load-pretrained-remote');
+    button.addEventListener('click', async () => {
+      const predictor = await new SentimentPredictor().init(HOSTED_URLS);
+      ui.prepUI(x => predictor.predict(x));
+    });
+    button.style.display = 'inline-block';
+  }
+
+  //// no local urls for now
+
+  // if (await loader.urlExists(LOCAL_URLS.model)) {
+  //   ui.status('Model available: ' + LOCAL_URLS.model);
+  //   const button = document.getElementById('load-pretrained-local');
+  //   button.addEventListener('click', async () => {
+  //     const predictor = await new SentimentPredictor().init(LOCAL_URLS);
+  //     ui.prepUI(x => predictor.predict(x));
+  //   });
+  //   button.style.display = 'inline-block';
+  // }
+  console.log("setupSentiment --> Standing by.")
+  // ui.status('Standing by.');
+}
+
+
+setupSentiment();
